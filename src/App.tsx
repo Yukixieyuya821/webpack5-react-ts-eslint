6cb4fc882962c6609c8cef228808cad1695e5387
yuki.xie <yu.xie@wiz.ai>
2022-09-19 17:55:06 +0800

    '配置vite'

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@
 import routes from './routes';
 import {renderRoutes, RouteConfig} from 'react-router-config';
 import styles from './style.module.scss';
+import bigImg from './assets/imgs/22kb.png';
 const App = () => (
-    <div className={styles.App}>
+    <div className="App">
         {/* 设置routes的类型为RouteConfig[]，否则报错 */}
         {renderRoutes(routes as RouteConfig[])}
-        Test
+        <h1 className={styles.title}>Test</h1>
+        <img src={bigImg} alt="大于于10kb的图片" />
     </div>
 );
 export default App;
